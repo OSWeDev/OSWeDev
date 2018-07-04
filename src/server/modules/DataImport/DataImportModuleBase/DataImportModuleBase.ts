e877ada60bda4c3e4e6e35ebc26ceda2fb5e40c1
Guillaume Luszack <guillaume@wedev.fr>
2018-07-04 11:26:42 +0200

    ajout du getter actif dans module importbase

--- src/server/modules/DataImport/DataImportModuleBase/DataImportModuleBase.ts
+++ src/server/modules/DataImport/DataImportModuleBase/DataImportModuleBase.ts
@@ -4,6 +4,7 @@ import IModuleBase from '../../../../shared/modules/IModuleBase';
 import IImportData from '../../../../shared/modules/DataImport/interfaces/IImportData';
 import IImportOptions from '../../../../shared/modules/DataImport/interfaces/IImportOptions';
 import DataImportHistoricVO from '../../../../shared/modules/DataImport/vos/DataImportHistoricVO';
+import Module from '../../../../shared/modules/Module';
 
 export default abstract class DataImportModuleBase implements IPostTraitementModule, IModuleBase {
 
@@ -15,9 +16,15 @@ export default abstract class DataImportModuleBase implements IPostTraitementMod
         ModulesManager.getInstance().registerModule(DataImportModuleBase.DataImportRoleName, this);
     }
 
+    An_accessor_cannot_be_declared_in_an_ambient_context_1086
+
     public registerApis() { }
     public initialize() { }
 
     public abstract hook_merge_imported_datas_in_database(datas: IImportData[], import_target_date_index: string, historic: DataImportHistoricVO, options: IImportOptions): Promise<boolean>;
     public abstract async hook_configure_import();
+
+    get actif(): boolean {
+        return (ModulesManager.getInstance().getModuleByNameAndRole(this.name, Module.SharedModuleRoleName) as Module).actif;
+    }
 }
