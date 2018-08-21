51b27a7011f60c2a0ae64eb03821e4302479273a
Julien Nesme <julien.nesme@francelink.fr>
2018-08-21 17:25:05 +0200

    pour l'instant impossible de faire marcher le vuejsclient dans oswedev ....

--- src/vuejsclient/ts/components/IDeclareVueComponent.ts
+++
@@ -1,3 +0,0 @@
-export default interface IDeclareVue {
-    $snotify: any;
-}
--- src/vuejsclient/ts/modules/IVueModule.ts
+++
@@ -1,6 +0,0 @@
-import { RouteConfig } from "vue-router/types/router";
-import IModuleBase from "../../../shared/modules/IModuleBase";
-
-export default interface IVueModule extends IModuleBase {
-  routes: RouteConfig[];
-}
