c47a80c955544b2fc7f73b9a63e6b1af96b48be8
Julien Nesme <julien.nesme@francelink.fr>
2018-12-31 16:53:49 +0100

    ajout constructeur de vo

--- src/shared/modules/ModuleTable.ts
+++ src/shared/modules/ModuleTable.ts
@@ -36,6 +36,8 @@ export default class ModuleTable<T extends IDistantVOBase> {
     public importable: boolean = false;
     public isModuleParamTable: boolean = false;
 
+    public voConstructor: () => T = null;
+
     constructor(
         tmp_module: Module,
         tmp_vo_type: string,
@@ -76,6 +78,17 @@ export default class ModuleTable<T extends IDistantVOBase> {
         if (this.vo_type) {
             VOsTypesManager.getInstance().registerModuleTable(this);
         }
+    }
+
+    public defineVOConstructor(voConstructor: () => T) {
+        this.voConstructor = voConstructor;
+    }
+
+    public getNewVO(): T {
+        if (this.voConstructor) {
+            return this.voConstructor();
+        }
+        return null;
     }
 
     public defineAsModuleParamTable(): ModuleTable<any> {
