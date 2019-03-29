3c998c3a5414bad43bcc04f4b1f4c2de3a5f2912
Julien Nesme <julien.nesme@francelink.fr>
2019-03-29 12:32:10 +0100

    debug prep

--- src/shared/modules/ProgramPlan/interfaces/IPlanRDVPrep.ts
+++ src/shared/modules/ProgramPlan/interfaces/IPlanRDVPrep.ts
@@ -3,5 +3,5 @@ import IDistantVOBase from '../../IDistantVOBase';
 export default interface IPlanRDVPrep extends IDistantVOBase {
     rdv_id: number;
     author_id: number;
-    cr_file_id: number;
+    prep_file_id: number;
 }
