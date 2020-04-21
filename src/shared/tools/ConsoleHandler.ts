5bf4bbaf5e6aa68f33ee63304614b4fc1881dbb3
Julien Nesme <julien@wedev.fr>
2020-04-21 13:13:56 +0200

    test nyc comments

--- src/shared/tools/ConsoleHandler.ts
+++ src/shared/tools/ConsoleHandler.ts
@@ -1,4 +1,5 @@
 const moment = require('moment');
+/* istanbul ignore next: not a usefull test to write */
 
 export default class ConsoleHandler {
 
@@ -17,27 +18,22 @@ export default class ConsoleHandler {
     }
 
     public error(error: string | Error): void {
-        /* istanbul ignore next: not a usefull test to write */
         console.error(this.get_text_msg(error));
     }
 
     public warn(error: string | Error): void {
-        /* istanbul ignore next: not a usefull test to write */
         console.warn(this.get_text_msg(error));
     }
 
     public log(error: string | Error): void {
-        /* istanbul ignore next: not a usefull test to write */
         console.log(this.get_text_msg(error));
     }
 
     private get_text_msg(error: string | Error): string {
-        /* istanbul ignore next: not a usefull test to write */
         return this.get_timestamp() + ConsoleHandler.SEPARATOR + (error ? (error as Error).message || error : error);
     }
 
     private get_timestamp(): string {
-        /* istanbul ignore next: not a usefull test to write */
         return moment().utc(true).format('YYYY-MM-DD HH:mm:ss.SSS');
     }
 }
