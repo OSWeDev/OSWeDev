61fe69743262340c11cea381f98eadce04418180
Julien Nesme <juliennesme@gmail.com>
2020-10-06 09:58:54 +0200

    merge master

--- src/shared/modules/Var/vos/VarDataValueResVO.ts
+++ src/shared/modules/Var/vos/VarDataValueResVO.ts
@@ -1,4 +1,5 @@
 import { Moment } from 'moment';
+import VarDataBaseVO from './VarDataBaseVO';
 
 export default class VarDataValueResVO {
     public static API_TYPE_ID: string = "vdvr";
@@ -13,4 +14,32 @@ export default class VarDataValueResVO {
     public value_ts: Moment;
 
     public constructor() { }
+
+    public set_from_vardata(vardata: VarDataBaseVO): VarDataValueResVO {
+        this.value = vardata.value;
+        this.index = vardata.index;
+        this.value_ts = vardata.value_ts;
+        this.value_type = vardata.value_type;
+        return this;
+    }
+
+    public set_value(value: number): VarDataValueResVO {
+        this.value = value;
+        return this;
+    }
+
+    public set_value_type(value_type: number): VarDataValueResVO {
+        this.value_type = value_type;
+        return this;
+    }
+
+    public set_value_ts(value_ts: Moment): VarDataValueResVO {
+        this.value_ts = value_ts;
+        return this;
+    }
+
+    public set_index(index: string): VarDataValueResVO {
+        this.index = index;
+        return this;
+    }
 }
