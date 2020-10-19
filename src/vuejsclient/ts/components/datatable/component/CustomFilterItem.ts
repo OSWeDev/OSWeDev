d740afd0dda15eb2df7345027507ce48a069f0d5
Michael DEBILLY <michael.debilly@atinov.fr>
2020-10-19 15:19:57 +0200

    Filtres Admin + Maj Field readonly

--- src/vuejsclient/ts/components/datatable/component/CustomFilterItem.ts
+++ src/vuejsclient/ts/components/datatable/component/CustomFilterItem.ts
@@ -3,5 +3,7 @@ export default class CustomFilterItem {
     public constructor(
         public label: string,
         public value: any,
-        public datatable_field_uid: string) { }
+        public datatable_field_uid: string,
+        public id: number
+    ) { }
 }
