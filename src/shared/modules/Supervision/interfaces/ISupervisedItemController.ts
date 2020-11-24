3f92069e484588320fb69fadb10dd8153dd3b9cc
Michael DEBILLY <michael.debilly@atinov.fr>
2020-11-24 18:40:17 +0100

    supervision

--- src/shared/modules/Supervision/interfaces/ISupervisedItemController.ts
+++ src/shared/modules/Supervision/interfaces/ISupervisedItemController.ts
@@ -5,6 +5,7 @@ export default interface ISupervisedItemController<T extends ISupervisedItem> {
 
     get_urls(supervised_item: T): ISupervisedItemURL[];
     get_description(supervised_item: T): string;
+    get_description_html(supervised_item: T): string;
     is_actif(): boolean;
     get_execute_time_ms(): number;
     work_all(): Promise<boolean>;
