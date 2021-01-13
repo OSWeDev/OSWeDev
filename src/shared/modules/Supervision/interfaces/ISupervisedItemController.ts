6146ca3111b83f8cf6cbb1ac1b78c27044dc5831
Julien Nesme <juliennesme@gmail.com>
2021-01-13 21:24:51 +0100

    merge master

--- src/shared/modules/Supervision/interfaces/ISupervisedItemController.ts
+++ src/shared/modules/Supervision/interfaces/ISupervisedItemController.ts
@@ -3,8 +3,6 @@ import ISupervisedItemURL from './ISupervisedItemURL';
 
 export default interface ISupervisedItemController<T extends ISupervisedItem> {
 
-    already_work: boolean;
-
     get_urls(supervised_item: T): ISupervisedItemURL[];
     get_description(supervised_item: T): string;
     get_description_html(supervised_item: T): string;
