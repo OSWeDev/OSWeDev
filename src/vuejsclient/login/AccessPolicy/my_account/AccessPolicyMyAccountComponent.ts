0dbf6e4d3e04644b3b6c609161967c35424c48bb
Julien Nesme <juliennesme@gmail.com>
2022-02-08 17:46:02 +0100

    save

--- src/vuejsclient/login/AccessPolicy/my_account/AccessPolicyMyAccountComponent.ts
+++ src/vuejsclient/login/AccessPolicy/my_account/AccessPolicyMyAccountComponent.ts
@@ -7,6 +7,8 @@ import './AccessPolicyMyAccountComponent.scss';
 import NFCUserTagListComponent from "../../../ts/components/NFCConnect/user_tag_list/NFCUserTagListComponent";
 import SessionShareComponent from "../../../ts/components/session_share/SessionShareComponent";
 import AccessPolicyMyAccountComponentController from "./AccessPolicyMyAccountComponentController";
+import VOsTypesManager from "../../../../shared/modules/VOsTypesManager";
+import SimpleDatatableField from "../../../../shared/modules/DAO/vos/datatable/SimpleDatatableField";
 
 @Component({
     template: require('./AccessPolicyMyAccountComponent.pug'),
@@ -30,5 +32,28 @@ export default class AccessPolicyMyAccountComponent extends VueComponentBase {
         if (!this.user) {
             this.$router.push('/');
         }
+    }
+
+    get user_moduletable() {
+        return VOsTypesManager.getInstance().moduleTables_by_voType[UserVO.API_TYPE_ID];
+    }
+
+    get editable_firstname() {
+        return new SimpleDatatableField('firstname').setModuleTable(this.user_moduletable);
+    }
+
+    get editable_lastname() {
+        return new SimpleDatatableField('lastname').setModuleTable(this.user_moduletable);
+    }
+
+    get editable_email() {
+        return new SimpleDatatableField('email').setModuleTable(this.user_moduletable);
+    }
+
+    get editable_phone() {
+        return new SimpleDatatableField('phone').setModuleTable(this.user_moduletable);
+    }
+
+    private async onchangevo() {
     }
 }
