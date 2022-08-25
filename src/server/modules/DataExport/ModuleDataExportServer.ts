c49ffbb1d4a551550c4d07a975c2c52086229714
Julien Nesme <juliennesme@gmail.com>
2022-08-25 10:27:24 +0200

    debug export dates

--- src/server/modules/DataExport/ModuleDataExportServer.ts
+++ src/server/modules/DataExport/ModuleDataExportServer.ts
@@ -660,17 +660,15 @@ export default class ModuleDataExportServer extends ModuleServerBase {
      * @returns Objet Date
      */
     private format_date_utc(timestamp: number): Date {
-        let date_locate = timestamp ? new Date(timestamp * 1000) : null;
-        if (date_locate) {
-            let timestmp_locate: number = Date.parse(date_locate.toDateString());
-            let diff_timestmp: number = (timestamp * 1000) - timestmp_locate;
-            let final_timestmp: number = timestmp_locate - diff_timestmp;
-            let date_white_timestmp_date_not_locate = new Date(final_timestmp);
-            return date_white_timestmp_date_not_locate;
-        } else {
-            return null;
+        if (timestamp) {
+            /**
+             * On soustrait le timezone local, et les 21 secondes pour Excel ... cf https://github.com/SheetJS/sheetjs/issues/2152 pour les 21 seconds en dur....
+             */
+            return new Date(Date.UTC(Dates.year(timestamp), Dates.month(timestamp), Dates.day(timestamp), 0, 0, 0) + ((new Date()).getTimezoneOffset() * 60 * 1000) - 21000);
         }
+        return null;
     }
+
     /**
      * Traduire le champs field.field_id de src_vo dans dest_vo dans l'optique d'un export excel
      * @param field le descriptif du champs à traduire
