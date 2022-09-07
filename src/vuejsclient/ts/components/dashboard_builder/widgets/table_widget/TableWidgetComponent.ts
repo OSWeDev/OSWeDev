3483fba1eab94cfc39c77e4559c0cc11492790e9
Florian-Argaud <80675391+Florian-Argaud@users.noreply.github.com>
2022-09-07 14:58:03 +0200

    correctif bug ToFixed ligne de total

--- src/vuejsclient/ts/components/dashboard_builder/widgets/table_widget/TableWidgetComponent.ts
+++ src/vuejsclient/ts/components/dashboard_builder/widgets/table_widget/TableWidgetComponent.ts
@@ -1080,7 +1080,10 @@ export default class TableWidgetComponent extends VueComponentBase {
             return;
         }
 
-        await this.reload_column_total();
+        if (this.widget_options.has_table_total_footer) {
+
+            await this.reload_column_total();
+        }
 
         this.loaded_once = true;
         this.is_busy = false;
