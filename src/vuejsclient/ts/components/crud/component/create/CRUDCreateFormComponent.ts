7e72c3bf5befce31d311393790706522caa2b7ab
k0raty <antonydavi338@gmail.com>
2022-10-18 12:32:36 +0200

    transfert des traduction entre table de widget terminé

--- src/vuejsclient/ts/components/crud/component/create/CRUDCreateFormComponent.ts
+++ src/vuejsclient/ts/components/crud/component/create/CRUDCreateFormComponent.ts
@@ -18,12 +18,20 @@ import VueComponentBase from '../../../VueComponentBase';
 import CRUDComponentManager from '../../CRUDComponentManager';
 import CRUDFormServices from '../CRUDFormServices';
 import "./CRUDCreateFormComponent.scss";
-//Copy_widget
+
+/*Import pour la copie d'un widget*/
 import { cloneDeep } from "lodash";
+import { query } from '../../../../../../shared/modules/ContextFilter/vos/ContextQueryVO';
 import DashboardPageVO from '../../../../../../shared/modules/DashboardBuilder/vos/DashboardPageVO';
 import InlineTranslatableText from '../../../InlineTranslatableText/InlineTranslatableText';
 import DashboardPageWidgetVO from '../../../../../../shared/modules/DashboardBuilder/vos/DashboardPageWidgetVO';
-import DashboardBuilderWidgetsController from '../../../dashboard_builder/widgets/DashboardBuilderWidgetsController';
+import DashboardWidgetVO from '../../../../../../shared/modules/DashboardBuilder/vos/DashboardWidgetVO';
+import { ModuleDroppableVoFieldsAction } from '../../../../components/dashboard_builder/droppable_vo_fields/DroppableVoFieldsStore';
+import TranslatableTextVO from '../../../../../../shared/modules/Translation/vos/TranslatableTextVO';
+import TranslationVO from '../../../../../../shared/modules/Translation/vos/TranslationVO';
+import DashboardBuilderController from '../../../../../../shared/modules/DashboardBuilder/DashboardBuilderController';
+import ModuleTranslation from "../../../../../../shared/modules/Translation/ModuleTranslation";
+import { ModuleTranslatableTextAction } from '../../../InlineTranslatableText/TranslatableTextStore';
 
 @Component({
     template: require('./CRUDCreateFormComponent.pug'),
@@ -33,6 +41,12 @@ import DashboardBuilderWidgetsController from '../../../dashboard_builder/widget
     },
 })
 export default class CRUDCreateFormComponent extends VueComponentBase {
+
+    @ModuleDroppableVoFieldsAction
+    private set_selected_fields: (selected_fields: { [api_type_id: string]: { [field_id: string]: boolean } }) => void;
+
+    @ModuleTranslatableTextAction
+    private set_flat_locale_translation: (translation: { code_text: string, value: string }) => void;
 
     @ModuleDAOGetter
     private getStoredDatas: { [API_TYPE_ID: string]: { [id: number]: IDistantVOBase } };
@@ -69,23 +83,6 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
     @Prop({ default: false })
     private show_placeholder: boolean;
 
-    @Prop({ default: null })
-    private copy_widget: boolean;
-
-    @Prop({ default: null })
-    private page_widget: DashboardPageWidgetVO;
-
-    @Prop({ default: null })
-    private pages: DashboardPageVO[];
-
-
-    @Prop({ default: null }) //TODO Trouver le bon default.
-    private page_id: number; //Page en cours
-
-    private copy_to_page: DashboardPageVO = null; //Page vers laquel on souhaite copier/déplacer
-
-    //Id de la page à selectionner par defaut.
-
     private editableVO: IDistantVOBase = null;
     private newVO: IDistantVOBase = null;
 
@@ -97,38 +94,22 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
     private crud_createDatatable_key: number = 0;
     private crud: CRUD<any> = null;
 
-    //copy_widget
+    /*Propriété pour la copie d'un widget */
+    @Prop({ default: null })
+    private copy_widget: boolean;
 
-    get page_to_copy_in_id(): number {
+    @Prop({ default: null })
+    private page_widget: DashboardPageWidgetVO;  //Contenant du widget à copier/déplacer
 
-        if (!this.page_id) {
-            return null;
-        }
-
-        if (this.pages.length < 2) {
-            return null;
-        }
-
-        if (this.copy_to_page == null) {
-            //Default case
-            let pagei = 0;
-
-            for (let i in this.pages) {
-                let page = this.pages[i];
-                if (page.id == this.page_id) {
-                    pagei = parseInt(i);
-                }
-            }
-            if (pagei == (this.pages.length - 1)) {
-                return this.pages[0].id;
-            }
-            return this.pages[pagei + 1].id;
-        } else {
-            return this.copy_to_page.id;
-        }
-    }
+    @Prop({ default: null })
+    private pages: DashboardPageVO[];
 
 
+    @Prop({ default: null })
+    private page_id: number; //Page en cours
+
+    private copy_to_page: DashboardPageVO = null; //Page vers laquel on souhaite copier/déplacer
+    private widget: DashboardWidgetVO = null; //Widget à copier/déplacer
 
     public update_key() {
         if (this.crud && (this.crud_createDatatable_key != this.crud.createDatatable.key)) {
@@ -137,54 +118,6 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
     }
 
 
-
-    private async do_transfert_widget() {
-        /*Déplace un widget d'un onglet vers un autre onglet*/
-
-        let widget_to_copy: DashboardPageWidgetVO = new DashboardPageWidgetVO();
-        widget_to_copy = cloneDeep(this.page_widget);
-
-
-        //Déplacement
-        delete widget_to_copy.id;
-        let to_which_page_id: number = this.page_to_copy_in_id;
-        widget_to_copy.page_id = to_which_page_id;
-
-        //Enregistrement du widget déplacé en base.
-        let id = await ModuleDAO.getInstance().insertOrUpdateVO(widget_to_copy);
-
-        //Suppression du widget (recharge la page par la même occasion)
-        this.$emit('suppress_widget'); //Si on refuse la suppression , le widget est tout de même copié.
-
-        //Fermeture de la modale
-
-        this.$emit('cancel');
-    }
-
-    private async do_copy_widget() {
-        /*Copie un widget dans un même onglet*/
-        let widget_to_copy: DashboardPageWidgetVO = new DashboardPageWidgetVO();
-        widget_to_copy = cloneDeep(this.page_widget);
-
-
-        //Déplacement
-        delete widget_to_copy.id;
-        let to_which_page_id: number = this.page_to_copy_in_id;
-
-
-        widget_to_copy.page_id = to_which_page_id;
-
-        //Enregistrement du widget déplacé en base.
-        let id = await ModuleDAO.getInstance().insertOrUpdateVO(widget_to_copy);
-
-        //reload des widgets si jamais les pages sont identiques
-        if (this.page_to_copy_in_id == this.page_id) {
-            this.$emit('reload_widgets');
-        }
-        //Fermeture de la modale
-        this.$emit('cancel');
-
-    }
     @Watch("api_type_id", { immediate: true })
     private async onchange_api_type_id() {
         if (!this.api_type_id) {
@@ -254,22 +187,6 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
         );
     }
 
-    //copy_widget
-    get pages_name_code_text(): string[] {
-        let res: string[] = [];
-
-        if (!this.pages) {
-            return res;
-        }
-
-        for (let i in this.pages) {
-            let page = this.pages[i];
-
-            res.push(page.translatable_name_code_text ? page.translatable_name_code_text : null);
-        }
-
-        return res;
-    }
 
 
     get CRUDTitle(): string {
@@ -282,11 +199,8 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
                 this.t(VOsTypesManager.getInstance().moduleTables_by_voType[this.crud.readDatatable.API_TYPE_ID].label.code_text)
         });
     }
-    //copy_widget
 
-    private select_page_to_copy_in(page: DashboardPageVO) {
-        this.copy_to_page = page;
-    }
+
 
 
     private async createVO() {
@@ -508,6 +422,198 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
 
     private async cancel() {
         this.$emit('cancel');
+    }
+
+    /*Fonctions utiles à la copie d'un widget */
+    get page_to_copy_in_id(): number {
+
+        if (!this.page_id) {
+            return null;
+        }
+
+        if (this.pages.length < 2) {
+            return null;
+        }
+
+        if (this.copy_to_page == null) {
+            //Default case
+            let pagei = 0;
+
+            for (let i in this.pages) {
+                let page = this.pages[i];
+                if (page.id == this.page_id) {
+                    pagei = parseInt(i);
+                }
+            }
+            if (pagei == (this.pages.length - 1)) {
+                return this.pages[0].id;
+            }
+            return this.pages[pagei + 1].id;
+        } else {
+            return this.copy_to_page.id;
+        }
+    }
+
+    get pages_name_code_text(): string[] {
+        let res: string[] = [];
+
+        if (!this.pages) {
+            return res;
+        }
+
+        for (let i in this.pages) {
+            let page = this.pages[i];
+
+            res.push(page.translatable_name_code_text ? page.translatable_name_code_text : null);
+        }
+
+        return res;
+    }
+
+
+    private find_page_by_id(find_page_id: number): DashboardPageVO {
+        /* Retourne la page correspondant à l'identifiant indiqué */
+
+        for (let i in this.pages) {
+            let page = this.pages[i];
+            if (page.id == find_page_id) {
+                return page;
+            }
+        }
+        console.log("Id de page introuvable !");
+    }
+
+    private async generate_i_and_weight(find_page_id: number, page_widget_to_copy: DashboardPageWidgetVO) {
+        /*
+        Retourne un i (identifient cellule pour griditem) existant parmis les widgets
+        de la page indiquée
+        */
+
+        //Identification de la page vers laquelle copier
+        let current_page: DashboardPageVO = this.find_page_by_id(find_page_id);
+        let this_page_widgets = await query(DashboardPageWidgetVO.API_TYPE_ID).filter_by_num_eq('page_id', current_page.id).select_vos<DashboardPageWidgetVO>();
+
+        //Eviter ces i afin d'avoir des cellules qui ont un identifiant griditem différent
+        let i_to_avoid: number[] = [];
+        let max_weight: number = 0;
+        for (let i in this_page_widgets) {
+            let widget = this_page_widgets[i];
+            i_to_avoid.push(widget.i);
+            if (widget.weight >= max_weight) {
+                max_weight = widget.weight + 1;
+            }
+        }
+
+        //Attribution d'un i et d'un weight cohérent au nouveau widget
+        page_widget_to_copy.i = i_to_avoid.reduce((a, b) => Math.max(a, b), -Infinity) + 1;
+        page_widget_to_copy.weight = max_weight;
+    }
+
+    private async transfert_trad(page_widget_to_copy_id: number) {
+        /* Permet de transférer ou copier les traductions d'un tableau (widget) vers un autre */
+
+        let page_widget_trads: TranslatableTextVO[] = await query(TranslatableTextVO.API_TYPE_ID).filter_by_text_starting_with('code_text', [
+            DashboardBuilderController.TableColumnDesc_NAME_CODE_PREFIX + this.page_widget.id + '.',
+            DashboardBuilderController.VOFIELDREF_NAME_CODE_PREFIX + this.page_widget.id + '.'
+        ]).select_vos<TranslatableTextVO>();
+
+        let page_widget_to_copy_trads: TranslatableTextVO[] = cloneDeep(page_widget_trads); //Copie des traductions
+
+
+        for (let j in page_widget_to_copy_trads) {
+            let page_widget_trad: TranslatableTextVO = page_widget_to_copy_trads[j];
+            //Changement des identifiants widget de ces trads.
+            let code = page_widget_trad.code_text;
+            // Text
+            let translations: TranslationVO[] = await ModuleDAO.getInstance().getVosByRefFieldIds<TranslationVO>(
+                TranslationVO.API_TYPE_ID, 'text_id', [page_widget_trad.id]);
+
+            delete page_widget_trad.id; //On supprime l'identifiant pour éviter les confusions
+
+            if (code.indexOf(DashboardBuilderController.TableColumnDesc_NAME_CODE_PREFIX + this.page_widget.id) == 0) {
+                page_widget_trad.code_text =
+                    DashboardBuilderController.TableColumnDesc_NAME_CODE_PREFIX +
+                    + page_widget_to_copy_id +
+                    code.substring((DashboardBuilderController.TableColumnDesc_NAME_CODE_PREFIX + page_widget_to_copy_id).length, code.length);
+            } else if (code.indexOf(DashboardBuilderController.VOFIELDREF_NAME_CODE_PREFIX + this.page_widget.id) == 0) {
+                page_widget_trad.code_text =
+                    DashboardBuilderController.VOFIELDREF_NAME_CODE_PREFIX +
+                    + page_widget_to_copy_id +
+                    code.substring((DashboardBuilderController.VOFIELDREF_NAME_CODE_PREFIX + page_widget_to_copy_id).length, code.length);
+            }
+
+            let insertOrDeleteQueryResulttt: InsertOrDeleteQueryResult = await ModuleDAO.getInstance().insertOrUpdateVO(page_widget_trad); //Ajout en base.
+            page_widget_trad.id = insertOrDeleteQueryResulttt.id;
+
+            page_widget_to_copy_trads[j] = page_widget_trad;
+
+            //Activation de la traduction
+            let menu_translation: TranslationVO = await ModuleTranslation.getInstance().getTranslation(translations[0].lang_id, page_widget_trad.id);
+            if (!menu_translation) {
+                menu_translation = new TranslationVO();
+                menu_translation.lang_id = translations[0].lang_id;
+                menu_translation.text_id = page_widget_trad.id;
+                menu_translation.translated = translations[0].translated;
+                let resi = await ModuleDAO.getInstance().insertOrUpdateVO(menu_translation);
+                if (resi && resi.id) {
+                    this.set_flat_locale_translation({
+                        code_text: page_widget_trad.code_text,
+                        value: translations[0].translated
+                    });
+                }
+            }
+
+        }
+
+    }
+
+    private async do_transfert_widget(copy_it: boolean = false) {
+        /*Déplace un widget d'un onglet vers un autre onglet*/
+
+
+        let page_widget_to_copy: DashboardPageWidgetVO = new DashboardPageWidgetVO();
+        page_widget_to_copy = cloneDeep(this.page_widget);
+
+
+        //Attribution d'un i et d'un poids cohérent au nouveau widget
+        await this.generate_i_and_weight(this.page_to_copy_in_id, page_widget_to_copy);
+
+        //Déplacement
+        delete page_widget_to_copy.id;
+        let to_which_page_id: number = this.page_to_copy_in_id;
+        page_widget_to_copy.page_id = to_which_page_id;
+
+        let insertOrDeleteQueryResult: InsertOrDeleteQueryResult = await ModuleDAO.getInstance().insertOrUpdateVO(page_widget_to_copy);
+
+        let page_widget_to_copy_id: number = insertOrDeleteQueryResult['id'];
+
+        //Transfert des traductions
+        if (this.page_widget._type == 'dashboard_pwidget') {
+            this.transfert_trad(page_widget_to_copy_id);
+        }
+
+        //Suppression du widget (recharge la page par la même occasion)
+        if (copy_it) {
+
+            this.$emit('reload_widgets');
+        } else {
+            this.$emit('suppress_widget'); //Si on refuse la suppression , le widget est tout de même copié.
+        }
+
+
+        //Fermeture de la modale
+
+        this.$emit('cancel');
+    }
+
+    private async do_copy_widget() {
+        /*Copie un widget d'un onglet vers un autre onglet*/
+
+        this.do_transfert_widget(true);
+    }
+
+    private select_page_to_copy_in(page: DashboardPageVO) {
+        this.copy_to_page = page;
     }
 
     private async suppress_widget() {
