5ef9d9c5d32f8b38e0b04b5d6404f546a54985d3
Michael Debilly <michael.debilly@francelink.fr>
2023-04-11 12:34:52 +0200

    modif patch pour que ça compile si tout est bon

--- src/generator/inits/premodules/CheckExtensions.ts
+++ src/generator/inits/premodules/CheckExtensions.ts
@@ -28,15 +28,37 @@ export default class CheckExtensions implements IGeneratorWorker {
      */
     public async work(db: IDatabase<any>) {
         try {
-            await db.query("CREATE EXTENSION IF NOT EXISTS btree_gist;");
-            await db.query("CREATE EXTENSION IF NOT EXISTS pgcrypto;");
-            await db.query("CREATE EXTENSION IF NOT EXISTS cube;");
-            await db.query("CREATE EXTENSION IF NOT EXISTS earthdistance;");
-            await db.query("ALTER ROLE " + ConfigurationService.node_configuration.BDD_OWNER + " NOSUPERUSER;");
+            let alter_role: number = 0;
+
+            alter_role += (await this.add_extension_if_not_exists(db, "btree_gist"));
+            alter_role += (await this.add_extension_if_not_exists(db, "pgcrypto"));
+            alter_role += (await this.add_extension_if_not_exists(db, "cube"));
+            alter_role += (await this.add_extension_if_not_exists(db, "earthdistance"));
+
+            if (alter_role > 0) {
+                await db.query("ALTER ROLE " + ConfigurationService.node_configuration.BDD_OWNER + " NOSUPERUSER;");
+            }
 
         } catch (error) {
             ConsoleHandler.error('Le rôle de la base doit être initialement configuré en superuser. Ce patch supprime ensuite le droit superuser. Les extensions suivantes sont obligatoires: "btree_gist", "pgcrypto", "cube", "earthdistance". Erreur: ' + error);
             throw new Error('Les extensions suivantes sont obligatoires: "btree_gist", "pgcrypto", "cube", "earthdistance".');
         }
+    }
+
+    public async add_extension_if_not_exists(db: IDatabase<any>, extname: string): Promise<number> {
+        try {
+            let ext_exist = await db.query("SELECT * FROM pg_extension WHERE extname='" + extname + "';");
+
+            if (ext_exist?.length > 0) {
+                return 0;
+            }
+
+            await db.query("CREATE EXTENSION IF NOT EXISTS " + extname + ";");
+        } catch (error) {
+            ConsoleHandler.error('Le rôle de la base doit être initialement configuré en superuser. Ce patch supprime ensuite le droit superuser. L\'extension suivante est obligatoire: "' + extname + '". Erreur: ' + error);
+            throw new Error('L\'extension suivante est obligatoire: "' + extname + '"');
+        }
+
+        return 1;
     }
 }
