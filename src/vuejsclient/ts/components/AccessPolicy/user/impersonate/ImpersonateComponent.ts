6a50eaef25e12522181d5cf009eb867dca0f225a
Michael Debilly <michael.debilly@francelink.fr>
2024-02-09 16:39:52 +0100

    bug impersonate (attente correctif JNE)

--- src/vuejsclient/ts/components/AccessPolicy/user/impersonate/ImpersonateComponent.ts
+++ src/vuejsclient/ts/components/AccessPolicy/user/impersonate/ImpersonateComponent.ts
@@ -24,9 +24,9 @@ export default class ImpersonateComponent extends VueComponentBase {
     private redirect_to: string = "/";
 
     get can_impersonate() {
-        if ((!this.vo) || this.vo.invalidated || this.vo.archived || this.vo.blocked || this.vo['user___invalidated'] || this.vo['user___archived'] || this.vo['user___blocked']) {
-            return false;
-        }
+        // if ((!this.vo) || this.vo.invalidated || this.vo.archived || this.vo.blocked || this.vo['user___invalidated'] || this.vo['user___archived'] || this.vo['user___blocked']) {
+        //     return false;
+        // }
 
         return true;
     }
