b17472fa2f1e65454b93398f5d31bc849f6ac5e0
Michael Debilly <michael.debilly@francelink.fr>
2025-01-21 15:30:44 +0100

    ajout crud file upload

--- src/vuejsclient/ts/components/crud/component/create/CRUDCreateFormComponent.ts
+++ src/vuejsclient/ts/components/crud/component/create/CRUDCreateFormComponent.ts
@@ -519,7 +519,12 @@ export default class CRUDCreateFormComponent extends VueComponentBase {
      * @param fileVo
      */
     private async uploadedFile_(vo: IDistantVOBase, field: DatatableField<any, any>, fileVo: FileVO) {
-        await CRUDFormServices.getInstance().uploadedFile(vo, field, fileVo, this.api_type_id, this.editableVO, this.updateData, this);
+        const to_refresh: boolean = await CRUDFormServices.getInstance().uploadedFile(vo, field, fileVo, this.api_type_id, this.editableVO, this.updateData, this);
+
+        if (to_refresh) {
+            this.crud.createDatatable.refresh();
+            this.crud_createDatatable_key = this.crud.createDatatable.key;
+        }
     }
 
     private async cancel() {
