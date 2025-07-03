5df01c380f77d29c461a14783e8e96508348b78b
Michael Debilly <michael.debilly@francelink.fr>
2025-07-03 16:50:22 +0200

    generate password user sur le front aussi

--- src/vuejsclient/ts/components/AccessPolicy/AccessPolicyVueController.ts
+++ src/vuejsclient/ts/components/AccessPolicy/AccessPolicyVueController.ts
@@ -2,9 +2,13 @@ import UserVO from '../../../../shared/modules/AccessPolicy/vos/UserVO';
 import ModuleDAO from '../../../../shared/modules/DAO/ModuleDAO';
 import CRUD from '../../../../shared/modules/DAO/vos/CRUD';
 import IDistantVOBase from '../../../../shared/modules/IDistantVOBase';
+import TextHandler from '../../../../shared/tools/TextHandler';
 import CRUDComponentManager from '../crud/CRUDComponentManager';
 
 export default class AccessPolicyVueController {
+
+    private static instance: AccessPolicyVueController = null;
+
     public static getInstance(): AccessPolicyVueController {
         if (!AccessPolicyVueController.instance) {
             AccessPolicyVueController.instance = new AccessPolicyVueController();
@@ -13,16 +17,15 @@ export default class AccessPolicyVueController {
         return AccessPolicyVueController.instance;
     }
 
-    private static instance: AccessPolicyVueController = null;
-
-    private constructor() {
-    }
-
     public async conf_precreate_uservo_unicity() {
 
         const user_crud: CRUD<UserVO> = CRUDComponentManager.getInstance().cruds_by_api_type_id[UserVO.API_TYPE_ID];
         user_crud.preCreate = this.dockeck.bind(this);
         user_crud.preUpdate = this.dockeck.bind(this);
+        user_crud.hook_prepare_new_vo_for_creation = async (vo: IDistantVOBase) => {
+            // On génère un mot de passe par défaut
+            (vo as UserVO).password = TextHandler.getInstance().generatePassword();
+        };
     }
 
     private async dockeck(dataVO: UserVO, ihmVO: IDistantVOBase) {
